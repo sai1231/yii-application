3cc3cdc132fb28032c67847eb81d8cc89d25e234
sai1231 <saishankar@live.in>
2016-07-21 08:23:47 +0530

    First Commmit

---
+++ backend/models/Departments.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "departments".
+ *
+ * @property integer $department_id
+ * @property integer $branch_id
+ * @property integer $company_id
+ * @property string $department_name
+ * @property string $department_created_date
+ * @property string $department_status
+ *
+ * @property Branches $branch
+ * @property Company $company
+ */
+class Departments extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'departments';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['branch_id', 'company_id', 'department_name', 'department_created_date', 'department_status'], 'required'],
+            [['branch_id', 'company_id'], 'integer'],
+            [['department_created_date'], 'safe'],
+            [['department_status'], 'string'],
+            [['department_name'], 'string', 'max' => 100],
+            [['branch_id'], 'exist', 'skipOnError' => true, 'targetClass' => Branches::className(), 'targetAttribute' => ['branch_id' => 'branch_id']],
+            [['company_id'], 'exist', 'skipOnError' => true, 'targetClass' => Company::className(), 'targetAttribute' => ['company_id' => 'company_id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'department_id' => 'Department ID',
+            'branch_id' => 'Branch ID',
+            'company_id' => 'Company ID',
+            'department_name' => 'Department Name',
+            'department_created_date' => 'Department Created Date',
+            'department_status' => 'Department Status',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBranch()
+    {
+        return $this->hasOne(Branches::className(), ['branch_id' => 'branch_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCompany()
+    {
+        return $this->hasOne(Company::className(), ['company_id' => 'company_id']);
+    }
+}
